e09655c36bd8cda4328c66d8f6e137ef4932b0a6
kondalgogada <48691133+kondalgogada@users.noreply.github.com>
2019-03-19 22:48:40 +0530

    Updated todo application

---
+++ script.js
@@ -0,0 +1,56 @@
+var myNodelist = document.getElementsByTagName("li");
+
+for (let i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("span");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+
+var close = document.getElementsByClassName("close");
+
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(e) {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('checked');
+  }
+}, false);
+
+
+function newElement() {
+  var li = document.createElement("li");
+  var input_value = document.getElementById("myInput").value;
+  var t = document.createTextNode(input_value);
+  li.appendChild(t);
+
+  if (input_value === '') {
+    alert("You must Enter something in the todo name!");
+  } else {
+    document.getElementById("myList").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
